c1d1cf52a35028cc8f5253ec57ba4fb2d490eaf2
Jin Tang <jintang413@gmail.com>
2022-02-20 11:55:56 -0500

    added templates

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,14 @@
+from turtle import title
+from flask import render_template
 from app import app
 
 
 @app.route("/")
 @app.route("/index")
 def index():
-    return "Hello, World! Jin"
+    user = {"username": "Summer"}
+    items = [
+        {"product": {"name": "SPEEDY BANDOULIÈRE 20"}, "status": True},
+        {"product": {"name": "PRECIOUS TIGER REYKJAVIK SCARF"}, "status": False},
+    ]
+    return render_template("index.html", title="Louis Vuitton", user=user, items=items)

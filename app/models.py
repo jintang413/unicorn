d446e6ba2ae74acf673e27cb0b110f41d2ca93ff
Jin Tang <jintang413@gmail.com>
2022-03-06 16:21:58 -0500

    user loader and password

--- app/models.py
+++ app/models.py
@@ -1,21 +1,34 @@
 from datetime import datetime
-from app import db
+from werkzeug.security import generate_password_hash, check_password_hash
+from app import db, login
+from flask_login import UserMixin
 import enum
 
 
 class MenuItemType(enum.Enum):
-    STARTER = 'Starter'
-    ENTREE = 'Entree'
-    DESSERT = 'Dessert'
+    STARTER = "Starter"
+    ENTREE = "Entree"
+    DESSERT = "Dessert"
 
-class User(db.Model):
+
+class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
     password_hash = db.Column(db.String(128))
 
     def __repr__(self):
-        return '<User {}>'.format(self.username)
+        return "<User {}>".format(self.username)
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
+@login.user_loader
+def load_user(id):
+    return User.query.get(int(id))
 
 class MenuItem(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -25,4 +38,4 @@ class MenuItem(db.Model):
     vegetarian = db.Column(db.Boolean())
 
     def __repr__(self):
-        return f'<Menu {self.name}>\n{self.description}'
+        return f"<Menu {self.name}>\n{self.description}"

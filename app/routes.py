c77a0dde46c2aa6c5a9b2c966cb740881914e145
Jin Tang <jintang413@gmail.com>
2022-03-06 20:01:42 -0500

    add last seen before request

--- app/routes.py
+++ app/routes.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from flask import flash, redirect, render_template, url_for
 from app import app, db
 from app.forms import LoginForm, RegistrationForm
@@ -66,3 +67,10 @@ def user(username):
         {"author": user, "body": "Test post #2"},
     ]
     return render_template("user.html", user=user, posts=posts)
+
+
+@app.before_request
+def before_request():
+    if current_user.is_authenticated:
+        current_user.last_seen = datetime.utcnow()
+        db.session.commit()
